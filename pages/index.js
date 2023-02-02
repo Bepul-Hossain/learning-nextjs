0c11767ac5e8df5c1e2917b94b8e9ffacccf02e2
Bepul-Hossain <bepul.cse.ru@gmail.com>
2023-02-02 20:49:29 +0600

    dynamic route

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Date from '../components/date';
 
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import {getSortedPostsData} from '../lib/posts';
 
-export default function Home() {
+export default function Home({allPostsData}) {
   return (
     // home === true
     <Layout home> 
@@ -17,6 +19,29 @@ export default function Home() {
       <h1>
           Read <Link href="/posts/first-post">first page</Link>
       </h1>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps(){
+    const allPostsData = getSortedPostsData()
+    return{
+      props: {
+        allPostsData
+      }
+    }
 }
